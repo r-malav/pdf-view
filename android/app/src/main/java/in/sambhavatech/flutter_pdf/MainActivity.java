3fb306ea528190292aac32aa24c8d1ac76acc9b3
r-malav <74281987+r-malav@users.noreply.github.com>
2021-01-08 11:00:33 +0530

    pdf view all

---
+++ android/app/src/main/java/in/sambhavatech/flutter_pdf/MainActivity.java
@@ -0,0 +1,6 @@
+package in.sambhavatech.flutter_pdf;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_plugin_pdf_viewer/flutter_plugin_pdf_viewer.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isLoading = true;
+  PDFDocument document;
+
+  @override
+  void initState() {
+    super.initState();
+    loadDocument();
+  }
+
+  loadDocument() async {
+    document = await PDFDocument.fromAsset('asserts/dart.pdf');
+    setState(() => _isLoading = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('FlutterPluginPDFViewer'),
+        ),
+        body: Center(
+            child: _isLoading
+                ? Center(child: CircularProgressIndicator())
+                : PDFViewer(document: document)),
+      ),
+    );
+  }
+}
